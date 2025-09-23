940a95084d7aee5584a32232ee1e97143ab17b9e
Allan <Allandesenvolvedor1@gmail.com>
2025-09-23 14:47:27 -0300

    Curso Laravel - Salvando dados no banco MySQL - #15

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
+Route::post('/events', [EventController::class, 'store']);
 
 Route::get('/contact', function() {
     return view('contact');
