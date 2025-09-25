a99d168f867d5df6ba3a0cc3d37e5ffc84f9cc46
Allan <Allandesenvolvedor1@gmail.com>
2025-09-25 15:24:22 -0300

    Curso Laravel - Busca no Laravel - #21

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,15 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use App\Models\Event;
 
 class EventController extends Controller
 {
-    public function index()
-    {
-        $events = Event::all();
-        
-        return view('welcome', ['events' => $events]);
+    
+    public function index() {
+
+        $search = request('search');
+
+        if($search) {
+
+            $events = Event::where([
+                ['title', 'like', '%'.$search.'%']
+            ])->get();
+
+        } else {
+            $events = Event::all();
+        }        
+    
+        return view('welcome',['events' => $events, 'search' => $search]);
+
     }
 
     public function create() {
@@ -19,6 +32,7 @@ class EventController extends Controller
     }
 
     public function store(Request $request) {
+
         $event = new Event;
 
         $event->title = $request->title;
@@ -28,30 +42,33 @@ class EventController extends Controller
         $event->description = $request->description;
         $event->items = $request->items;
 
-
+        // Image Upload
         if($request->hasFile('image') && $request->file('image')->isValid()) {
+
             $requestImage = $request->image;
 
             $extension = $requestImage->extension();
+
             $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
 
             $requestImage->move(public_path('img/events'), $imageName);
 
             $event->image = $imageName;
+
         }
 
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
+
     }
 
     public function show($id) {
+
         $event = Event::findOrFail($id);
 
         return view('events.show', ['event' => $event]);
+        
     }
 
-
 }
-
-
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,18 @@
 @section('content')
     <div id="search-container" class="col-md-12">
         <h1>Busque um evento</h1>
-        <form action="">
+        <form action="/" method="GET">
             <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
         </form>
     </div>
     <div id="events-container" class="col-md-12">
-        <h2>Proximos Eventos</h2>
-        <p class="subtitle">Veja os eventos dos proximos eventos</p>
+        @if($search)
+            <h2>Buscando por: {{$search}}</h2>
+            @else
+            <h2>Proximos Eventos</h2>
+            <p class="subtitle">Veja os eventos dos proximos eventos</p>
+        @endif
+        
         <div id="cards-container" class="row">
             @foreach($events as $event)
             <div class="card col-md-3">
@@ -24,8 +29,10 @@
                 </div>
             </div>
             @endforeach
-            @if(count($events) == 0 )
-                <p>Nao ha eventos disponiveis! <br> Esperamos que voce crie o primeiro evento!</p>
+            @if(count($events) == 0  && $search)
+                <p>Nao foi possivel encontrar nenhum evento com <strong>{{$search}}</strong>! <a href="/">Voltar para a Home</a> </p>
+            @elseif(count($events) == 0)
+                <p>Não há eventos disponíveis</p>
             @endif
         </div>
     </div>
